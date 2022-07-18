55e97e45326d5e52b0d624ec7ca0d262f0af977e
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-07-18 18:37:24 +0200

    Implementing Logging and Warnings

--- src/Logging.cs
+++ src/Logging.cs
@@ -1,8 +1,36 @@
 class Logging
 {
+    private static StreamWriter? sw;
     public void LogPosition(PositionChangedEventArgs args)
     {
-
+        if(sw!=null)
+        {
+            sw.Write($"_____\nPosition {args.Time} : N{args.Position.longitude} W{args.Position.latitude}");
+        } else 
+        {
+            Console.WriteLine("Error! Logging not possible.");
+        }
     }
-    public void LogWarning(RiskEventArgs)
+    public void LogWarning(RiskEventArgs args)
+    {
+        if(sw!=null)
+        {
+            sw.WriteLine($"----------Lawinengefahr.{args.Time}");
+            sw.WriteLine($"Risiko für einen Lawinenabgang: {Enum.GetName(args.RiskLevel)}");
+            sw.WriteLine($"Besonders folgende Schneeprobleme: ");
+            foreach (SnowProblem item in args.SnowProblems)
+            {
+                sw.WriteLine($"   - {Enum.GetName(item)}");
+            }
+            sw.WriteLine("----------");
+        } else 
+        {
+            Console.WriteLine("Error! Logging not possible.");
+        }
+    }
+
+    public Logging(string filepath)
+    {
+        sw = new StreamWriter(filepath); 
+    }
 }
