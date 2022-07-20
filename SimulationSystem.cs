fb8a7afffa1dd96e149f859d9a8185cc3a6e0ea7
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-07-20 15:42:24 +0200

    divided WorkingPosition and SimulationSystem files

--- SimulationSystem.cs
+++ SimulationSystem.cs
@@ -1,15 +1,3 @@
-struct WorkingPosition
-{
-    public int time;  // the time in ms, that the system rests at this position
-    public Position Position;
-
-    public WorkingPosition(int time, int x, int y)
-    {
-        this.time=time;
-        Position=new Position(x, y);
-    }
-}
-
 class SimulationSystem
 { 
     private static AvalancheWarnSystem? myWarnSystem;
