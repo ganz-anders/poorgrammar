49c6d3d6d572ceca5c3941dbd21fa351599ff805
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-07-21 21:12:30 +0200

    commenting Position, updating ClassDiagram

--- src/Position.cs
+++ src/Position.cs
@@ -2,11 +2,11 @@ struct Position
 {
     public int longitude; //x (east-west)
     public int latitude; //y (north-south)
-    public override string ToString()
+    public override string ToString()   //To String Method for easy Console/StreamWriter Output
     {
         return "N"+longitude.ToString()+" O"+latitude.ToString();
     }
-    public Position(int x, int y)
+    public Position(int x, int y)   //Constructor for easy use of the struct
     {
         longitude=x;
         latitude=y;
