0449b997e5c176041140029500893fb505da4c5f
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-07-21 10:49:01 +0200

    commenting map

--- src/Map.cs
+++ src/Map.cs
@@ -5,22 +5,24 @@ class Map
     private Position NWReference; //Reference point at the North-West End of the map
     private Position SEReference; //Reference point at the South-East End of the map
     private string UTMZoneReference;
-    private double[][] MapData; //like in a matrix the first value is the y value and the second is the x
-    public bool PositionOnMap(Position position)
+    private double[][] MapData; // !!! like a matrix the first value is the y value (latitude) and the second is the x (logitude)
+    public bool PositionOnMap(Position position)    //checks if position is between the reference positions (=>on map)
     {
         return NWReference.longitude<position.longitude&&position.longitude<SEReference.longitude
         &&NWReference.latitude<position.latitude&&position.latitude<SEReference.latitude;
     }
-    public int getGradient(Position position)
+    public int getGradient(Position position)   //calculates the Gradient at the position
     {
-        if (!PositionOnMap(position))
+        if (!PositionOnMap(position))           //Exception if position out of map
         {
             throw new Exception("Out of the Map.");
         }
-        double xGrad, yGrad, Grad;
-        double x=(position.longitude-NWReference.longitude)/Grid;
+
+        // linear aproximation in two directions
+        double xGrad, yGrad, Grad;              
+        double x=(position.longitude-NWReference.longitude)/Grid;               //the value of the position relativ to the NWReference point ->MapData entry nr.
         double y=(position.latitude-NWReference.latitude)/Grid;
-        double P11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)];
+        double P11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)];       //four auxilary points (the next point on the grid in each direction)
         double P12=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)];
         double P22=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)+1];
         double P21=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)+1];
@@ -28,28 +30,28 @@ class Map
         yGrad=(((P21-P11)/Grid)+((P22-P12)/Grid))/2;
         xGrad=(((P12-P11)/Grid)+((P22-P21)/Grid))/2;
 
-        yGrad=Math.Abs(yGrad);
+        yGrad=Math.Abs(yGrad);                          //only the absolut values ,bc. gradient can be in every direction
         xGrad=Math.Abs(xGrad);
 
-        Grad=Math.Sqrt(xGrad*xGrad+yGrad*yGrad);
+        Grad=Math.Sqrt(xGrad*xGrad+yGrad*yGrad);        //accumulatin the gradient in x and y direction
         
-        Grad=Math.Atan(Grad)*(180/Math.PI);
+        Grad=Math.Atan(Grad)*(180/Math.PI);             //returning the gradient in degree
 
         Grad=Math.Round(Grad);
 
 
         return((int)Grad);
     }
-    public Direction? getDirection(Position position)
+    public Direction? getDirection(Position position)       //calculates the exposition of the handed over position based on the gradient
     {
         if (!PositionOnMap(position))
         {
             throw new Exception("Out of the Map.");
         }
         double xGrad, yGrad;
-        double x=(position.longitude-NWReference.longitude)/Grid;
+        double x=(position.longitude-NWReference.longitude)/Grid;                   //the value of the position relativ to the NWReference point ->MapData entry nr.
         double y=(position.latitude-NWReference.latitude)/Grid;
-        double P11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)];
+        double P11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)];           //four auxilary points (the next point on the grid in each direction)
         double P12=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)];
         double P22=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)+1];
         double P21=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)+1];
@@ -57,7 +59,7 @@ class Map
         yGrad=(((P21-P11)/Grid)+((P22-P12)/Grid))/2;
         xGrad=(((P12-P11)/Grid)+((P22-P21)/Grid))/2;
 
-        switch (xGrad)
+        switch (xGrad)                              //analyzing the gradient in x and y direction
         {
             case >0.5f: //West-Exposition
                 switch (yGrad)
@@ -99,9 +101,9 @@ class Map
             throw new Exception("Out of the Map.");
         }
         double R1,R2,P;
-        double x=(position.longitude-NWReference.longitude)/Grid;
+        double x=(position.longitude-NWReference.longitude)/Grid;               //the value of the position relativ to the NWReference point ->MapData entry nr.
         double y=(position.latitude-NWReference.latitude)/Grid;
-        double Q11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)+1];
+        double Q11=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)+1];     //four auxilary points (the next point on the grid in each direction)
         double Q12=MapData[(int)Math.Truncate(x)][(int)Math.Truncate(y)];
         double Q22=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)];
         double Q21=MapData[(int)Math.Truncate(x)+1][(int)Math.Truncate(y)+1];
@@ -109,28 +111,28 @@ class Map
         //R1=Q11+((Q21-Q11)/Grid)*(x-Math.Truncate(x));
         //R2=Q12+((Q22-Q12)/Grid)*(x-Math.Truncate(x));
 
-        R1=(((Math.Truncate(x)+Grid-x)/Grid)*Q11+((x-Math.Truncate(x))/Grid)*Q21);
+        R1=(((Math.Truncate(x)+Grid-x)/Grid)*Q11+((x-Math.Truncate(x))/Grid)*Q21);          //linear interpolation between Q11,Q21 and Q12, Q22
         R2=(((Math.Truncate(x)+Grid-x)/Grid)*Q12+((x-Math.Truncate(x))/Grid)*Q22);
 
         //P=R1+((R2-R1)/Grid)*(y-Math.Truncate(y));
 
-        P=-(((Math.Truncate(y)-y)/(Grid))*R1 + ((y-(Math.Truncate(y)+Grid))/(Grid))*R2);
+        P=-(((Math.Truncate(y)-y)/(Grid))*R1 + ((y-(Math.Truncate(y)+Grid))/(Grid))*R2);    //second linear interpolation between R1 and R2
 
         P=Math.Round(P);
 
         return (int)P;
 
     }
-    public Map()
+    public Map()        //map constructor
     {
-        const string fileaddress="data/map.txt";
+        const string fileaddress="data/map.txt";        //const fileaddres for the map file
         string? buffer;
         string[] inputs;
         int xsize=0, ysize=0;
         StreamReader sr;
 
         NumberFormatInfo provider = new NumberFormatInfo();
-        provider.NumberDecimalSeparator = ".";
+        provider.NumberDecimalSeparator = ".";                  //cultureInfo important for correct converting further down
 
         Console.WriteLine("Karte einlesen...");
         try
@@ -145,7 +147,7 @@ class Map
         
         try
         {
-            buffer=sr.ReadLine();
+            buffer=sr.ReadLine();           //reading the Reference point
             if (buffer!=null)
             {
                 inputs=buffer.Split(' ');
@@ -156,7 +158,7 @@ class Map
             {
                 throw new Exception();
             }
-            buffer=sr.ReadLine();
+            buffer=sr.ReadLine();               //reading the map size
             if (buffer!=null)
             {
                 inputs=buffer.Split(',');
@@ -172,12 +174,12 @@ class Map
                 throw new Exception();
             }
 
-            MapData= new double[xsize][];
+            MapData= new double[xsize][];       //map is new array of arrays
             for (int i = 0; i < xsize; i++)
             {
                 MapData[i]=new double[ysize];
             }
-            for (int i = 0; i < ysize; i++)
+            for (int i = 0; i < ysize; i++)     //reading the map
             {
                 buffer=sr.ReadLine();
                 if (buffer!=null)
@@ -193,7 +195,7 @@ class Map
                 }
             }
 
-            SEReference.longitude=NWReference.longitude+(int)Grid*xsize;
+            SEReference.longitude=NWReference.longitude+(int)Grid*xsize;        //calculatiing the second Reference point
             SEReference.latitude=NWReference.latitude+(int)Grid*ysize;
         }
         catch (System.Exception)
